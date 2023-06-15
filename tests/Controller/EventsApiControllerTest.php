7ddf873b2b903fbf3fb217474746cb0ad3ab47d5
Chris Balla <ballachris13@gmail.com>
2023-06-15 02:49:23 +0200

    minor improvment in testing

--- tests/Controller/EventsApiControllerTest.php
+++ tests/Controller/EventsApiControllerTest.php
@@ -17,6 +17,20 @@ class EventsApiControllerTest extends WebTestCase
         // Add assertions to verify the expected data in the $responseData array
         $this->assertIsArray($responseData);
 
+        foreach ($responseData as $event) {
+            $this->assertArrayHasKey('id', $event);
+            $this->assertArrayHasKey('title', $event);
+            $this->assertArrayHasKey('datestr', $event);
+
+            $this->assertNotEmpty($event['id']);
+            $this->assertNotEmpty($event['title']);
+            $this->assertNotEmpty($event['datestr']);
+
+            // assertions based on the expected values of each event
+            $this->assertIsInt($event['id']);
+            $this->assertIsString($event['title']);
+            $this->assertIsString($event['datestr']);
+        }
     }
 
     public function testNew()
@@ -30,10 +44,30 @@ class EventsApiControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
         $responseData = json_decode($client->getResponse()->getContent(), true);
-        // Add assertions to verify the expected data in the $responseData array
         $this->assertArrayHasKey('id', $responseData);
         $this->assertArrayHasKey('title', $responseData);
         $this->assertArrayHasKey('datestr', $responseData);
+
+        // Additional assertions based on the expected data
+        $this->assertNotEmpty($responseData['id']);
+        $this->assertEquals('Test Event', $responseData['title']);
+        $this->assertEquals('2023-06-13', $responseData['datestr']);
+
+        // Retrieve the created event from the API to verify its details
+        $createdEventId = $responseData['id'];
+        $client->request('GET', '/api/events/' . $createdEventId);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $createdEventData = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('id', $createdEventData);
+        $this->assertArrayHasKey('title', $createdEventData);
+        $this->assertArrayHasKey('datestr', $createdEventData);
+
+        // Additional assertions to verify the details of the created event
+        $this->assertEquals($createdEventId, $createdEventData['id']);
+        $this->assertEquals('Test Event', $createdEventData['title']);
+        $this->assertEquals('2023-06-13', $createdEventData['datestr']);
+
 
     }
 
